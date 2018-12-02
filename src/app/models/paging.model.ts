b252fff52f208f0ade45531f1589a77c816eb410
SeungkeunHyun <mysuccess@daum.net>
2018-12-02 10:00:36 +0900

    better styling on modals

--- src/app/models/paging.model.ts
+++ src/app/models/paging.model.ts
@@ -2,13 +2,13 @@ export class Paging {
 	public pageNumber: number;
 	public pageSize: number;
 	public totalItems: number;
-  public totalPages: number;
-  public pageArray: <number>[];
+	public totalPages: number;
+	public pageArray: number[];
 	constructor(modelList: any[]) {
-    this.pageNumber = 1;
-    this.pageSize = 5;
+		this.pageNumber = 1;
+		this.pageSize = 5;
 		this.totalItems = modelList.length;
-    this.totalPages = Math.ceil(this.totalItems / (this.pageSize + 0.0));
-    this.pageArray = Array(this.totalPages).fill().map((x,i) => i+1);
-  }
+		this.totalPages = Math.ceil(this.totalItems / (this.pageSize + 0.0));
+		this.pageArray = Array(this.totalPages).fill(null).map((x, i) => i + 1);
+	}
 }
