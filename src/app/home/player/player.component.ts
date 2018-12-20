2186cbd29e0b039976aa161c715fd8c0aadb4ba1
승근 현 <sk@mynameismira.com>
2018-12-20 18:14:55 +0900

    added controllers on PlayerComponent

--- src/app/home/player/player.component.ts
+++ src/app/home/player/player.component.ts
@@ -4,29 +4,76 @@ import { Store } from '@ngrx/store';
 import { Episode } from './../../_models/episode.model';
 import { Cast } from './../../_models/cast.model';
 import { Observable, Subscription } from 'rxjs';
-import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import * as $ from 'jquery';
+
+import {
+	Component,
+	OnInit,
+	ViewChild,
+	OnDestroy,
+	AfterViewInit
+} from '@angular/core';
 import * as selectors from '../../_store/cast.selectors';
 
 @Component({
 	selector: 'app-player',
 	templateUrl: './player.component.html',
-	styleUrls: [ './player.component.css' ]
+	styleUrls: ['./player.component.css']
 })
-export class PlayerComponent implements OnInit, OnDestroy {
+export class PlayerComponent implements OnInit, OnDestroy, AfterViewInit {
 	cast$: Observable<Cast>;
 	episode: Episode;
 	loaded = false;
 	subs: Subscription;
 	@ViewChild('player') player;
-	constructor(private store: Store<AppState>, private playService: EpisodePlayerService) {}
+	constructor(
+		private store: Store<AppState>,
+		private playService: EpisodePlayerService
+	) {}
 
 	ngOnInit() {
-		this.subs = this.playService.subject.subscribe((ep) => {
+		this.subs = this.playService.subject.subscribe(ep => {
 			this.episode = ep;
-			this.cast$ = this.store.select(selectors.getCastById(this.episode.castID));
+			this.cast$ = this.store.select(
+				selectors.getCastById(this.episode.castID)
+			);
 			this.loaded = true;
-			console.log(this.episode, this.player);
 		});
+	}
+
+	ngAfterViewInit() {
+		const buttons = document.querySelectorAll('button.btn');
+		console.log(buttons);
+	}
+
+	controlPlayer(ctrl) {
+		console.log(ctrl, this.player);
+		const pl = this.player.nativeElement;
+		switch (ctrl) {
+			case 'fb':
+				pl.currentTime -= 30;
+				break;
+			case 'b':
+				pl.currentTime -= 5;
+				break;
+			case 'p':
+				if (pl.paused) {
+					pl.play();
+					$('#ctrlP i').removeClass('fa-play');
+					$('#ctrlP i').addClass('fa-pause');
+				} else {
+					pl.pause();
+					$('#ctrlP i').removeClass('fa-pause');
+					$('#ctrlP i').addClass('fa-play');
+				}
+				break;
+			case 'f':
+				pl.currentTime += 5;
+				break;
+			case 'ff':
+				pl.currentTime += 30;
+				break;
+		}
 	}
 
 	ngOnDestroy() {
