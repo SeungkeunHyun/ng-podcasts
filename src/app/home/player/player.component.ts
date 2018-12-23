b66e2063ae080472a8c4b7ab557cb8d20e9b0303
승근 현 <sk@mynameismira.com>
2018-12-23 14:29:45 +0900

    deleted debug log

--- src/app/home/player/player.component.ts
+++ src/app/home/player/player.component.ts
@@ -25,205 +25,203 @@ export class PlayerComponent implements OnInit, OnDestroy, AfterViewInit {
   subs: Subscription;
   durpipe = new DurationPipe();
   controllers = ['fb', 'b', 'p', 'f', 'ff'];
-	constructor(
-		private store: Store<AppState>,
-		private playService: EpisodePlayerService
-	) {
-		this.percent = new Subject<number>();
-	}
+  constructor(
+    private store: Store<AppState>,
+    private playService: EpisodePlayerService
+  ) {
+    this.percent = new Subject<number>();
+  }
 
-	ngAfterViewInit() {
-		console.log('after view init');
-		this.setupPlayer();
-	}
+  ngAfterViewInit() {
+    console.log('after view init');
+    this.setupPlayer();
+  }
 
-	ngOnInit() {
-		this.subs = this.playService.subject.subscribe(ep => {
-			this.episode = ep;
-			this.cast$ = this.store.select(
-				selectors.getCastById(this.episode.castID)
-			);
-			this.loaded = true;
-			if (!this.player) {
-				if (this.episode.mediaURL.indexOf('.mp3') > -1) {
-					this.player = document.createElement('audio');
-				} else {
-					this.player = document.createElement('video');
-				}
-				this.player.onloadeddata = () => {
-					this.setupPlayer();
-				};
-			}
-			this.preparePlayer();
-		});
-	}
+  ngOnInit() {
+    this.subs = this.playService.subject.subscribe(ep => {
+      this.episode = ep;
+      this.cast$ = this.store.select(
+        selectors.getCastById(this.episode.castID)
+      );
+      this.loaded = true;
+      if (!this.player) {
+        if (this.episode.mediaURL.indexOf('.mp3') > -1) {
+          this.player = document.createElement('audio');
+        } else {
+          this.player = document.createElement('video');
+        }
+        this.player.onloadeddata = () => {
+          this.setupPlayer();
+        };
+      }
+      this.preparePlayer();
+    });
+  }
 
-	getProgressPct(): number {
-		return Math.floor((this.player.currentTime / this.player.duration) * 100);
-	}
+  getProgressPct(): number {
+    return Math.floor((this.player.currentTime / this.player.duration) * 100);
+  }
 
-	preparePlayer() {
-		this.player.src = this.episode.mediaURL;
-		this.player.preload = 'true';
-		const hist = this.getHistory(this.episode.id);
-		console.log(hist, this.player);
-		if (hist) {
-			this.player.currentTime = hist.pausedAt;
-		}
-		$('#ctrlP i')
-			.removeClass('fa-play')
-			.addClass('fa-pause');
-		this.player.play();
-	}
+  preparePlayer() {
+    this.player.src = this.episode.mediaURL;
+    this.player.preload = 'true';
+    const hist = this.getHistory(this.episode.id);
+    console.log(hist, this.player);
+    if (hist) {
+      this.player.currentTime = hist.pausedAt;
+    }
+    $('#ctrlP i')
+      .removeClass('fa-play')
+      .addClass('fa-pause');
+    this.player.play();
+  }
 
-	setupPlayer() {
-		$('#divPlayer').empty();
-		$('#divPlayer').append(this.player);
-		// $('#divProgress').on('click', this.movePlaytime);
-		console.log('setup player');
-		if (!this.player) {
-			return;
-		}
-		console.log('player', this.player);
-		this.player.onplay = () => {
-			console.log('player started!');
-		};
-		this.player.ontimeupdate = () => {
-			const pct = this.getProgressPct();
-			const $pgbar = $('#divProgress');
-			$pgbar.css('width', pct + '%');
-			$pgbar.text(pct + '%');
-		};
-		this.player.onended = () => {
-			this.deleteEndedEpisode();
-		};
-		this.player.onpause = () => {
-			console.log('player paused');
-			$('#ctrlP i')
-				.removeClass('fa-pause')
-				.addClass('fa-play');
-			const history = {
-				episode: this.episode,
-				pausedAt: this.player.currentTime,
-				storedAt: new Date().getTime()
-			};
-			this.storePausedEpisode(history);
-		};
-		this.player.onabort = () => {
-			const history = {
-				episode: this.episode,
-				pausedAt: this.player.currentTime,
-				storedAt: new Date().getTime()
-			};
-			this.storePausedEpisode(history);
-		};
-	}
+  setupPlayer() {
+    $('#divPlayer').empty();
+    $('#divPlayer').append(this.player);
+    // $('#divProgress').on('click', this.movePlaytime);
+    console.log('setup player');
+    if (!this.player) {
+      return;
+    }
+    console.log('player', this.player);
+    this.player.onplay = () => {
+      console.log('player started!');
+    };
+    this.player.ontimeupdate = () => {
+      const pct = this.getProgressPct();
+      const $pgbar = $('#divProgress');
+      $pgbar.css('width', pct + '%');
+      $pgbar.text(pct + '%');
+    };
+    this.player.onended = () => {
+      this.deleteEndedEpisode();
+    };
+    this.player.onpause = () => {
+      console.log('player paused');
+      $('#ctrlP i')
+        .removeClass('fa-pause')
+        .addClass('fa-play');
+      const history = {
+        episode: this.episode,
+        pausedAt: this.player.currentTime,
+        storedAt: new Date().getTime()
+      };
+      this.storePausedEpisode(history);
+    };
+    this.player.onabort = () => {
+      const history = {
+        episode: this.episode,
+        pausedAt: this.player.currentTime,
+        storedAt: new Date().getTime()
+      };
+      this.storePausedEpisode(history);
+    };
+  }
 
-	deleteEndedEpisode() {
-		const strBookmarks = localStorage.getItem('bookmarks');
-		let bookmarks = {};
-		if (!strBookmarks) {
-			return;
-		}
-		bookmarks = JSON.parse(strBookmarks);
-		delete bookmarks[this.episode.id];
-		localStorage.setItem('bookmarks', JSON.stringify(bookmarks, null, 2));
-	}
+  deleteEndedEpisode() {
+    const strBookmarks = localStorage.getItem('bookmarks');
+    let bookmarks = {};
+    if (!strBookmarks) {
+      return;
+    }
+    bookmarks = JSON.parse(strBookmarks);
+    delete bookmarks[this.episode.id];
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks, null, 2));
+  }
 
-	getCursorTime(e) {
-		const pbar: HTMLElement = document.querySelector('#divProgressWrapper');
-		const ctime = this.player.duration * (e.offsetX / pbar.offsetWidth);
-		pbar.title = this.durpipe.transform(ctime);
-	}
+  getCursorTime(e) {
+    const pbar: HTMLElement = document.querySelector('#divProgressWrapper');
+    const ctime = this.player.duration * (e.offsetX / pbar.offsetWidth);
+    pbar.title = this.durpipe.transform(ctime);
+  }
 
-	movePlaytime(e) {
-		const pbar: HTMLElement = document.querySelector('#divProgressWrapper');
-		console.log(pbar);
-		const ctime = this.player.duration * (e.offsetX / pbar.offsetWidth);
-		this.player.currentTime = ctime;
-	}
+  movePlaytime(e) {
+    const pbar: HTMLElement = document.querySelector('#divProgressWrapper');
+    const ctime = this.player.duration * (e.offsetX / pbar.offsetWidth);
+    this.player.currentTime = ctime;
+  }
 
-	getHistory(id) {
-		const strBookmarks = localStorage.getItem('bookmarks');
-		if (strBookmarks) {
-			const bookmarks = JSON.parse(strBookmarks);
-			return bookmarks[id];
-		}
-		return null;
-	}
+  getHistory(id) {
+    const strBookmarks = localStorage.getItem('bookmarks');
+    if (strBookmarks) {
+      const bookmarks = JSON.parse(strBookmarks);
+      return bookmarks[id];
+    }
+    return null;
+  }
 
-	storePausedEpisode(hist) {
-		const strBookmarks = localStorage.getItem('bookmarks');
-		let bookmarks = {};
-		if (!strBookmarks) {
-			bookmarks = {};
-		} else {
-			bookmarks = JSON.parse(strBookmarks);
-		}
-		bookmarks[hist.episode.id] = hist;
-		localStorage.setItem('bookmarks', JSON.stringify(bookmarks, null, 2));
-	}
+  storePausedEpisode(hist) {
+    const strBookmarks = localStorage.getItem('bookmarks');
+    let bookmarks = {};
+    if (!strBookmarks) {
+      bookmarks = {};
+    } else {
+      bookmarks = JSON.parse(strBookmarks);
+    }
+    bookmarks[hist.episode.id] = hist;
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks, null, 2));
+  }
 
-	getIcon(c: string) {
-		let iconClass = 'fa fa-';
-		switch (c) {
-			case 'fb':
-				iconClass += 'fast-backward';
-				break;
-			case 'b':
-				iconClass += 'backward';
-				break;
-			case 'p':
-				iconClass += 'pause';
-				break;
-			case 'f':
-				iconClass += 'forward';
-				break;
-			case 'ff':
-				iconClass += 'fast-forward';
-				break;
-		}
-		return iconClass;
-	}
+  getIcon(c: string) {
+    let iconClass = 'fa fa-';
+    switch (c) {
+      case 'fb':
+        iconClass += 'fast-backward';
+        break;
+      case 'b':
+        iconClass += 'backward';
+        break;
+      case 'p':
+        iconClass += 'pause';
+        break;
+      case 'f':
+        iconClass += 'forward';
+        break;
+      case 'ff':
+        iconClass += 'fast-forward';
+        break;
+    }
+    return iconClass;
+  }
 
-	controlPlayer(ctrl) {
-		console.log(ctrl, this.player);
-		switch (ctrl) {
-			case 'fb':
-				this.player.currentTime -= 30;
-				break;
-			case 'b':
-				this.player.currentTime -= 5;
-				break;
-			case 'p':
-				if (this.player.paused) {
-					this.player.play();
-					$('#ctrlP i').removeClass('fa-play');
-					$('#ctrlP i').addClass('fa-pause');
-				} else {
-					this.player.pause();
-					$('#ctrlP i').removeClass('fa-pause');
-					$('#ctrlP i').addClass('fa-play');
-				}
-				break;
-			case 'f':
-				this.player.currentTime += 5;
-				break;
-			case 'ff':
-				this.player.currentTime += 30;
-				break;
-		}
-	}
+  controlPlayer(ctrl) {
+    switch (ctrl) {
+      case 'fb':
+        this.player.currentTime -= 30;
+        break;
+      case 'b':
+        this.player.currentTime -= 5;
+        break;
+      case 'p':
+        if (this.player.paused) {
+          this.player.play();
+          $('#ctrlP i').removeClass('fa-play');
+          $('#ctrlP i').addClass('fa-pause');
+        } else {
+          this.player.pause();
+          $('#ctrlP i').removeClass('fa-pause');
+          $('#ctrlP i').addClass('fa-play');
+        }
+        break;
+      case 'f':
+        this.player.currentTime += 5;
+        break;
+      case 'ff':
+        this.player.currentTime += 30;
+        break;
+    }
+  }
 
-	ngOnDestroy() {
-		if (this.player) {
-			const hist = {
-				episode: this.episode,
-				pausedAt: this.player.currentTime
-			};
-			console.log(hist);
-			this.storePausedEpisode(hist);
-		}
-		this.subs.unsubscribe();
-	}
+  ngOnDestroy() {
+    if (this.player) {
+      const hist = {
+        episode: this.episode,
+        pausedAt: this.player.currentTime
+      };
+      console.log(hist);
+      this.storePausedEpisode(hist);
+    }
+    this.subs.unsubscribe();
+  }
 }
