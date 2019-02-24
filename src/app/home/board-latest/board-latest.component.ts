f6463f6ee73b7804f3d56816a1b0677d6868acf8
승근 현 <sk@mynameismira.com>
2019-02-24 11:15:26 +0900

    added latest ep desc sort

--- src/app/home/board-latest/board-latest.component.ts
+++ src/app/home/board-latest/board-latest.component.ts
@@ -8,47 +8,55 @@ import { AppState } from 'src/app/_store/app.reducer';
 import * as selectors from '../../_store/cast.selectors';
 
 @Component({
-  selector: 'app-board-latest',
-  templateUrl: './board-latest.component.html',
-  styleUrls: ['./board-latest.component.css']
+	selector: 'app-board-latest',
+	templateUrl: './board-latest.component.html',
+	styleUrls: ['./board-latest.component.css']
 })
 export class BoardLatestComponent implements OnInit, OnDestroy {
-  latestEpisodes: Episode[];
-  subs: Subscription[] = [];
-  ep_label = 'Latest';
-  constructor(
-    private store: Store<AppState>,
-    private playService: EpisodePlayerService,
-    private elasticService: ElasticService
-  ) {
-    this.subs.push(
-      this.elasticService.episodes.subscribe(episodes => {
-        this.latestEpisodes = episodes;
-      })
-    );
-    this.subs.push(
-      this.elasticService.ep_label.subscribe(
-        ep_label => (this.ep_label = ep_label)
-      )
-    );
-  }
+	latestEpisodes: Episode[];
+	subs: Subscription[] = [];
+	ep_label = 'Latest';
+	constructor(
+		private store: Store<AppState>,
+		private playService: EpisodePlayerService,
+		private elasticService: ElasticService
+	) {
+		this.subs.push(
+			this.elasticService.episodes.subscribe(episodes => {
+				this.latestEpisodes = episodes;
+			})
+		);
+		this.subs.push(
+			this.elasticService.ep_label.subscribe(
+				ep_label => (this.ep_label = ep_label)
+			)
+		);
+	}
 
-  ngOnInit() {
-    const sub = this.store.select('casts').subscribe(casts => {
-      this.store.select(selectors.selectLatestEpisodes).subscribe(eps => {
-        this.latestEpisodes = eps;
-      });
-    });
-    this.subs.push(sub);
-  }
+	ngOnInit() {
+		const sub = this.store.select('casts').subscribe(casts => {
+			this.store.select(selectors.selectLatestEpisodes).subscribe(eps => {
+				eps.sort((a, b) => {
+					if (a.pubDate > b.pubDate) {
+						return -1;
+					} else if (a.pubDate === b.pubDate) {
+						return 0;
+					}
+					return 1;
+				});
+				this.latestEpisodes = eps;
+			});
+		});
+		this.subs.push(sub);
+	}
 
-  playCast(ep) {
-    this.playService.setEpisode(ep);
-  }
+	playCast(ep) {
+		this.playService.setEpisode(ep);
+	}
 
-  ngOnDestroy() {
-    for (const sub of this.subs) {
-      sub.unsubscribe();
-    }
-  }
+	ngOnDestroy() {
+		for (const sub of this.subs) {
+			sub.unsubscribe();
+		}
+	}
 }
